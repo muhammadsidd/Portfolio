8c22e9196e202c32e890d6adbca3b4b348eaf760
muhammadsidd <muhammadsidd@mail.usf.edu>
2021-06-14 23:20:25 -0400

    detailed skills added

--- base/views.py
+++ base/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-
+from .models import Project, Tag, Skill
 # Create your views here.
 
 def homePage(request):
-    return render(request, 'base/home.html')
+    projects = Project.objects.all()
+    skills = Skill.objects.all()
+    return render(request, 'base/home.html', {'projects':projects, 'skills':skills})
